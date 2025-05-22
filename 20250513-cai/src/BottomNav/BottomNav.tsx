6686ae0b628b54cd5ad5f9b10432e4174219d069
UncleDoubleU <alexmozagba@gmail.com>
2025-05-22 11:37:39 +0200

    bottom nav latest styling

--- 20250513-cai/src/BottomNav/BottomNav.tsx
+++ 20250513-cai/src/BottomNav/BottomNav.tsx
@@ -3,39 +3,42 @@ import styles from "./BottomNav.module.sass";
 function BottomNav() {
   return (
     <nav role="search bar" className={styles.container}>
-      <ul>
+      <section className={styles.searchToolCont}>
         <h3>filters</h3>
-        <li>
-          <a href="#">filter 1</a>
-        </li>
-        <li>
-          <a href="#">filter 2</a>
-        </li>
-        <li>
-          <a href="#">filter 3</a>
-        </li>
-        <li>
-          <a href="#">filter 4</a>
-        </li>
-      </ul>
-
-      <ul>
-        <h3>sort by</h3>
-        <li>
-          <a href="#">filter 5</a>
-        </li>
-        <li>
-          <a href="#">filter 6</a>
-        </li>
-        <li>
-          <a href="#">filter 7</a>
-        </li>
-        <li>
-          <a href="#">filter 8</a>
-        </li>
-      </ul>
-
-      <input type="text" placeholder="Search" />
+        <ul>
+          <li>
+            <a href="#">filter 1</a>
+          </li>
+          <li>
+            <a href="#">filter 2</a>
+          </li>
+          <li>
+            <a href="#">filter 3</a>
+          </li>
+          <li>
+            <a href="#">filter 4</a>
+          </li>
+        </ul>
+      </section>
+      <section className={styles.searchToolCont}>
+        <ul>
+          <h3>sort by</h3>
+          <li>
+            <a href="#">filter 5</a>
+          </li>
+          <li>
+            <a href="#">filter 6</a>
+          </li>
+          <li>
+            <a href="#">filter 7</a>
+          </li>
+          <li>
+            <a href="#">filter 8</a>
+          </li>
+        </ul>
+      </section>
+      <button>search tools</button>
+      <input name="search bar" type="text" placeholder="search" />
     </nav>
   );
 }
