e75e726f3d3f2acb689ef3b591c117c95d3933c8
Walu <walu.mozagba@gmail.com>
2025-05-22 13:19:45 +0200

    bottom nav - semantic improvement to the list elements

--- 20250513-cai/src/BottomNav/BottomNav.tsx
+++ 20250513-cai/src/BottomNav/BottomNav.tsx
@@ -1,41 +1,49 @@
+import { useState } from "react";
 import styles from "./BottomNav.module.sass";
 
 function BottomNav() {
+
+  const [isClicked, setIsClicked] = useState("")                     
+
   return (
     <nav role="search bar" className={styles.container}>
       <section className={styles.searchToolCont}>
+        <article>
         <h3>filters</h3>
-        <ul>
-          <li>
-            <a href="#">filter 1</a>
-          </li>
-          <li>
-            <a href="#">filter 2</a>
-          </li>
-          <li>
-            <a href="#">filter 3</a>
-          </li>
-          <li>
-            <a href="#">filter 4</a>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <a href="#">filter 1</a>
+            </li>
+            <li>
+              <a href="#">filter 2</a>
+            </li>
+            <li>
+              <a href="#">filter 3</a>
+            </li>
+            <li>
+              <a href="#">filter 4</a>
+            </li>
+          </ul>
+        </article>
       </section>
       <section className={styles.searchToolCont}>
-        <ul>
-          <h3>sort by</h3>
-          <li>
-            <a href="#">filter 5</a>
-          </li>
-          <li>
-            <a href="#">filter 6</a>
-          </li>
-          <li>
-            <a href="#">filter 7</a>
-          </li>
-          <li>
-            <a href="#">filter 8</a>
-          </li>
-        </ul>
+        <article>
+          <ul>
+            <h3>sort by</h3>
+            <li>
+              <a href="#">filter 5</a>
+            </li>
+            <li>
+              <a href="#">filter 6</a>
+            </li>
+            <li>
+              <a href="#">filter 7</a>
+            </li>
+            <li>
+              <a href="#">filter 8</a>
+            </li>
+          </ul>
+        </article>
       </section>
       <button>search tools</button>
       <input name="search bar" type="text" placeholder="search" />
