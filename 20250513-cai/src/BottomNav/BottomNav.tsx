3b96682a01074d1eb49457afd12f8c2b1b37a658
Walu <walu.mozagba@gmail.com>
2025-05-28 14:30:47 +0200

    bn - started working on the larger screen sizes and added the options

--- 20250513-cai/src/BottomNav/BottomNav.tsx
+++ 20250513-cai/src/BottomNav/BottomNav.tsx
@@ -6,19 +6,48 @@ import SearchTool from "../SearchTool/SearchTool";
 function BottomNav() {
 
   const [isClicked, setIsClicked] = useState(false)
-  const [btnText, setBtnText] = useState("search tools");
+  const [btnText, setBtnText] = useState("tools");
 
   useEffect(() => {
-    setBtnText(!isClicked ? "search tools" : "close")
+    setBtnText(!isClicked ? "tools" : "close")
   }, [isClicked])
 
   const filters = [
-    { id: 0, title: "by genre" },
-    { id: 1, title: "by date of birth" },
-    { id: 2, title: "by place of birth" },
-    { id: 3, title: "first published in" },
-    { id: 4, title: "by languages" },
-    { id: 5, title: " by publishers" },
+    { id: 0, title: "by genre", options: ["men", "women", "non-binary", "trans", "other"] },
+    { id: 1, title: "by date of birth", options: [1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000] },
+    {
+      id: 2, title: "by place of birth", options: [
+        "Lower Uele",
+        "Equateur",
+        "East Kasai",
+        "Ituri",
+        "Kasai",
+        "Kasai - Central",
+        "Kinshasa",
+        "Kongo Central",
+        "Kwango",
+        "Kwilu",
+        "Lomami",
+        "Lualaba",
+        "Mai - Ndombe",
+        "Maniema",
+        "Mongala",
+        "North Kivu",
+        "North Ubangi",
+        "Sankuru",
+        "South Kivu",
+        "South Ubangi",
+        "Tanganyika",
+        "Tshopo",
+        "Tshuapa",
+        "Upper Katanga",
+        "Upper Lomami",
+        "Upper Uele"
+      ]
+    },
+    { id: 3, title: "first published in", options: [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020] },
+    { id: 4, title: "by languages", options: ["English", "French", "Lingala", "Tshiluba", "Kikongo", "Swahili", "German", "Dutch"] },
+    { id: 5, title: " by publishers", options: ["publisher 1", "publisher 2", "publisher 3", "publisher 4"] },
   ];
   const sorting = [
     { id: 0, title: "alphabetical" },
